af8f966f391f823aea518e2c23fe9cf24dbc886e
Christopher Hedley <chris@cghsystems.net>
2012-07-18 18:38:39 +0100

    Added support for calling create and delete rest services

--- src/test/groovy/net/cghsystems/definitions/client/ws/RestConsumer.groovy
+++ src/test/groovy/net/cghsystems/definitions/client/ws/RestConsumer.groovy
@@ -12,6 +12,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mortbay.jetty.Server
 import org.mortbay.jetty.webapp.WebAppContext
+import org.springframework.http.HttpStatus
 import org.springframework.integration.channel.DirectChannel
 import org.springframework.integration.core.MessageHandler
 import org.springframework.integration.support.MessageBuilder
@@ -19,15 +20,28 @@ import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
 
+/**
+ * Integration test that will Spin up an instance of Jetty, run cgh-definitions-servives 
+ * war and perform the REST service operations to test the SI configuration
+ * 
+ * @author chris
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner)
 @ContextConfiguration("classpath:META-INF/spring/definitions-services-client-si-context.xml")
 class RestConsumer {
 
     @Resource(name = "findDefinitionsRequestChannel")
-    DirectChannel requestChannel
+    DirectChannel findDefinitionsRequestChannel
 
     @Resource(name = "findDefinitionsReplyChannel")
-    DirectChannel responseChannel
+    DirectChannel findDefinitionsReplyChannel
+
+    @Resource(name = "createDefinitionsRequestChannel")
+    DirectChannel createDefinitionsRequestChannel
+
+    @Resource(name = "deleteDefinitionsRequestChannel")
+    DirectChannel deleteDefinitionsRequestChannel
 
     /** Jetty Server */
     static server
@@ -53,13 +67,35 @@ class RestConsumer {
         server.stop()
     }
 
+    /**
+     * Given a {@link Definition}
+     * When Create Then Should Create
+     * And When Find Then Should Find
+     * And When Delete Then Should Delete
+     */
     @Test
-    void shouldConsume() {
-        final expected = new Definition("InsertTestDefinitionData-1", "test-name", "test-def", "test-desc", 49)
+    void shouldCreateAndThenFindAndThenDeleteDefinitionFromRemoteRestService() {
+        def expected = new Definition("RestConsumer-1", "RestConsumer", "RestConsumer", "RestConsumer", 49)
+
+        //Create
+        createDefinitionsRequestChannel.send(MessageBuilder.withPayload(expected).build())
+
+
+        //Find
+        //Ensures that any returned payload matches the expected.
         final handle = {
-            assert it.getPayload() == expected : "Expected matching Definitions"
+            def actual = it.getPayload()
+            assert  actual == expected : "Expected matching Definitions"
         }
-        responseChannel.subscribe([handleMessage: handle] as  MessageHandler)
-        requestChannel.send(MessageBuilder.withPayload(expected.id).build())
+        findDefinitionsReplyChannel.subscribe([handleMessage: handle] as  MessageHandler)
+        //Call the remote Rest service
+        findDefinitionsRequestChannel.send(MessageBuilder.withPayload(expected.id).build())
+
+
+        //Delete
+        final id = expected.id
+        deleteDefinitionsRequestChannel.send(MessageBuilder.withPayload(id).build())
+        expected = HttpStatus.OK //Change type of expected as the original definition should have been deleted
+        findDefinitionsRequestChannel.send(MessageBuilder.withPayload(id).build())
     }
 }
